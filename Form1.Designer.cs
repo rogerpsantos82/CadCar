73a4184c4fe80138a94fc8ceb37a468681565c7c
Roger <rogerpsantos@yahoo.com.br>
2019-06-18 00:05:15 -0300

    Upload do Projeto

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace CadCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.txtCor = new System.Windows.Forms.TextBox();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.cmbNovo = new System.Windows.Forms.ComboBox();
+            this.txtRegistro = new System.Windows.Forms.TextBox();
+            this.txtAtualizacao = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(12, 12);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.BtnCadastrar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(93, 12);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 1;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.BtnAlterar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(174, 12);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.BtnExcluir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(12, 41);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(501, 23);
+            this.btnConsultar.TabIndex = 3;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.BtnConsultar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(438, 12);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Location = new System.Drawing.Point(357, 113);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(75, 23);
+            this.btnConsulta.TabIndex = 5;
+            this.btnConsulta.Text = "Consulta";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.BtnConsulta_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(438, 113);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.BtnFechar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Marca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Modelo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Cor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ano";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Preço";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Descrição";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Novo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 280);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Data de Cadastro";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 307);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Data de Atualização";
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(138, 90);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(213, 20);
+            this.txtMarca.TabIndex = 16;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(138, 116);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(213, 20);
+            this.txtModelo.TabIndex = 17;
+            // 
+            // txtCor
+            // 
+            this.txtCor.Location = new System.Drawing.Point(138, 142);
+            this.txtCor.Name = "txtCor";
+            this.txtCor.Size = new System.Drawing.Size(213, 20);
+            this.txtCor.TabIndex = 18;
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(138, 168);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(213, 20);
+            this.txtAno.TabIndex = 19;
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(138, 194);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(213, 20);
+            this.txtPreco.TabIndex = 20;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(138, 220);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDesc.Size = new System.Drawing.Size(213, 20);
+            this.txtDesc.TabIndex = 21;
+            // 
+            // cmbNovo
+            // 
+            this.cmbNovo.FormattingEnabled = true;
+            this.cmbNovo.Items.AddRange(new object[] {
+            "Sim",
+            "Não"});
+            this.cmbNovo.Location = new System.Drawing.Point(138, 246);
+            this.cmbNovo.Name = "cmbNovo";
+            this.cmbNovo.Size = new System.Drawing.Size(121, 21);
+            this.cmbNovo.TabIndex = 22;
+            // 
+            // txtRegistro
+            // 
+            this.txtRegistro.Location = new System.Drawing.Point(138, 273);
+            this.txtRegistro.Name = "txtRegistro";
+            this.txtRegistro.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRegistro.Size = new System.Drawing.Size(213, 20);
+            this.txtRegistro.TabIndex = 23;
+            // 
+            // txtAtualizacao
+            // 
+            this.txtAtualizacao.Location = new System.Drawing.Point(138, 300);
+            this.txtAtualizacao.Name = "txtAtualizacao";
+            this.txtAtualizacao.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtAtualizacao.Size = new System.Drawing.Size(213, 20);
+            this.txtAtualizacao.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 350);
+            this.Controls.Add(this.txtAtualizacao);
+            this.Controls.Add(this.txtRegistro);
+            this.Controls.Add(this.cmbNovo);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.txtCor);
+            this.Controls.Add(this.txtModelo);
+            this.Controls.Add(this.txtMarca);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "Form1";
+            this.Text = "Cadastro de Veículos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.TextBox txtCor;
+        private System.Windows.Forms.TextBox txtAno;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.ComboBox cmbNovo;
+        private System.Windows.Forms.TextBox txtRegistro;
+        private System.Windows.Forms.TextBox txtAtualizacao;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadCar
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConexao = null;
+        private string strConexao = "Server=localhost\\SQLEXPRESS;Database=CADCARDB;Trusted_Connection=True;";
+        private string strSQL = string.Empty;
+        private int idCar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ResetaCampos();
+        }
+
+        private void ResetaCampos()
+        {
+            btnCadastrar.Enabled = true;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnConsultar.Enabled = true;
+            btnConsulta.Visible = false;
+            btnConsulta.Enabled = false;
+            btnFechar.Visible = false;
+            btnFechar.Enabled = false;
+
+            txtMarca.Clear();
+            txtModelo.Clear();
+            txtCor.Clear();
+            txtAno.Clear();
+            txtPreco.Clear();
+            txtDesc.Clear();
+            txtRegistro.Clear();
+            txtAtualizacao.Clear();
+
+            txtRegistro.Enabled = false;
+            txtAtualizacao.Enabled = false;
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnCadastrar_Click(object sender, EventArgs e)
+        {
+            strSQL = "insert into VEICULO( MARCA , MODELO , COR , ANO , PRECO , DESCR , NOVO , DTREGISTRO , DTATUALIZACAO ) values( @MARCA , @MODELO , @COR , @ANO , @PRECO , @DESCR , @NOVO , CURRENT_TIMESTAMP , CURRENT_TIMESTAMP )";
+            sqlConexao = new SqlConnection(strConexao);
+            SqlCommand eftvComando = new SqlCommand(strSQL, sqlConexao);
+
+            eftvComando.Parameters.Add("@MARCA", SqlDbType.VarChar).Value = txtMarca.Text;
+            eftvComando.Parameters.Add("@MODELO", SqlDbType.VarChar).Value = txtModelo.Text;
+            eftvComando.Parameters.Add("@COR", SqlDbType.VarChar).Value = txtCor.Text;
+            eftvComando.Parameters.Add("@ANO", SqlDbType.VarChar).Value = txtAno.Text;
+            eftvComando.Parameters.Add("@PRECO", SqlDbType.Decimal).Value = txtPreco.Text;
+            eftvComando.Parameters.Add("@DESCR", SqlDbType.VarChar).Value = txtDesc.Text;
+
+            if (cmbNovo.Text == "Sim")
+                eftvComando.Parameters.Add("@NOVO", SqlDbType.Bit).Value = 1;
+            else
+                eftvComando.Parameters.Add("@NOVO", SqlDbType.Bit).Value = 0;
+
+            try
+            {
+                sqlConexao.Open();
+                eftvComando.ExecuteNonQuery();
+                MessageBox.Show("CADASTRO EFETUADO COM SUCESSO!");
+
+                ResetaCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConexao.Close();
+            }
+        }
+
+        private void BtnConsultar_Click(object sender, EventArgs e)
+        {
+            ResetaCampos();
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = false;
+            btnExcluir.Enabled = false;
+            btnConsultar.Enabled = false;
+            btnConsulta.Visible = true;
+            btnConsulta.Enabled = true;
+            btnFechar.Visible = true;
+            btnFechar.Enabled = true;
+        }
+
+        private void BtnConsulta_Click(object sender, EventArgs e)
+        {
+            btnCadastrar.Enabled = false;
+            btnAlterar.Enabled = true;
+            btnExcluir.Enabled = true;
+            btnConsultar.Enabled = true;
+            btnConsulta.Visible = false;
+            btnConsulta.Enabled = false;
+            btnFechar.Visible = true;
+            btnFechar.Enabled = true;
+
+            strSQL = "select * from VEICULO where MODELO LIKE @txtPesquisa";
+            sqlConexao = new SqlConnection(strConexao);
+            SqlCommand eftvComando = new SqlCommand(strSQL, sqlConexao);
+
+            eftvComando.Parameters.Add("@txtPesquisa", SqlDbType.VarChar).Value = "%"+txtModelo.Text+"%";
+
+            try
+            {
+                sqlConexao.Open();
+                SqlDataReader regRecuperado = eftvComando.ExecuteReader();
+                regRecuperado.Read();
+
+                idCar = Convert.ToInt32(regRecuperado["ID"]);
+                txtMarca.Text = Convert.ToString(regRecuperado["MARCA"]);
+                txtModelo.Text = Convert.ToString(regRecuperado["MODELO"]);
+                txtCor.Text = Convert.ToString(regRecuperado["COR"]);
+                txtAno.Text = Convert.ToString(regRecuperado["ANO"]);
+                txtPreco.Text = Convert.ToString(regRecuperado["PRECO"]);
+                txtDesc.Text = Convert.ToString(regRecuperado["DESCR"]);
+                txtRegistro.Text = Convert.ToString(regRecuperado["DTREGISTRO"]);
+                txtAtualizacao.Text = Convert.ToString(regRecuperado["DTATUALIZACAO"]);
+
+                if (Convert.ToInt32(regRecuperado["NOVO"]) == 1)
+                    cmbNovo.Text = "Sim";
+                else
+                    cmbNovo.Text = "Não";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ResetaCampos();
+            }
+            finally
+            {
+                sqlConexao.Close();
+            }
+
+        }
+
+        private void BtnFechar_Click(object sender, EventArgs e)
+        {
+            ResetaCampos();
+        }
+
+        private void BtnAlterar_Click(object sender, EventArgs e)
+        {
+
+            strSQL = "update VEICULO set MARCA = @MARCA , MODELO = @MODELO , COR = @COR , ANO = @ANO , PRECO = @PRECO , DESCR = @DESCR , NOVO = @NOVO , DTATUALIZACAO = CURRENT_TIMESTAMP WHERE ID = @IDCAR ";
+            sqlConexao = new SqlConnection(strConexao);
+            SqlCommand eftvComando = new SqlCommand(strSQL, sqlConexao);
+
+            eftvComando.Parameters.Add("@IDCAR", SqlDbType.Int).Value = idCar;
+            eftvComando.Parameters.Add("@MARCA", SqlDbType.VarChar).Value = txtMarca.Text;
+            eftvComando.Parameters.Add("@MODELO", SqlDbType.VarChar).Value = txtModelo.Text;
+            eftvComando.Parameters.Add("@COR", SqlDbType.VarChar).Value = txtCor.Text;
+            eftvComando.Parameters.Add("@ANO", SqlDbType.VarChar).Value = txtAno.Text;
+            eftvComando.Parameters.Add("@PRECO", SqlDbType.Decimal).Value = txtPreco.Text;
+            eftvComando.Parameters.Add("@DESCR", SqlDbType.VarChar).Value = txtDesc.Text;
+
+            if (cmbNovo.Text == "Sim")
+                eftvComando.Parameters.Add("@NOVO", SqlDbType.Int).Value = 1;
+            else
+                eftvComando.Parameters.Add("@NOVO", SqlDbType.Int).Value = 0;
+
+            try
+            {
+                sqlConexao.Open();
+                eftvComando.ExecuteNonQuery();
+                MessageBox.Show("ALTERAÇÃO REALIZADA COM SUCESSO!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConexao.Close();
+            }
+        }
+
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+
+            strSQL = "delete from VEICULO where id = @idCar";
+            sqlConexao = new SqlConnection(strConexao);
+            SqlCommand eftvComando = new SqlCommand(strSQL, sqlConexao);
+
+            eftvComando.Parameters.Add("@idCar", SqlDbType.Int).Value = idCar;
+
+            try
+            {
+                sqlConexao.Open();
+                eftvComando.ExecuteNonQuery();
+                MessageBox.Show("REGISTRO EXCLUÍDO COM SUCESSO!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConexao.Close();
+            }
+
+            ResetaCampos();
+
+        }
+    }
+}
